82e4febb4ac797271116da4ebdc4a30a5aa1c08e
Poornima Rao <poornima@khyaal.com>
2025-12-24 13:36:51 +0530

    updated package.swift

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "KhyaalCoreSDK_iOS",
+    name: "KhyaalCoreSDK",
     platforms: [
         .iOS(.v15),
         .macOS(.v12)
@@ -10,13 +10,13 @@ let package = Package(
     products: [
         // This exposes your framework as a library to be imported by users.
         .library(
-            name: "KhyaalCoreSDK_iOS",
+            name: "KhyaalCoreSDK",
             targets: ["KhyaalCoreSDK"]
         )
     ],
     targets: [
         .binaryTarget(
-            name: "KhyaalCoreSDK_iOS",
+            name: "KhyaalCoreSDK",
             url: "https://github.com/Khyaal-Inc/KhyaalCoreSDK/releases/download/sdks/KhyaalCoreSDK_iOS_v1.0.0.xcframework.zip,
             checksum: "50c0bb8a84152ac6877e6b9f06eaf8f1126f110afa583c1f0b181edcc1028c8c"
         )
