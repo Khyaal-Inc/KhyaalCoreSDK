e12026528acdb795f869273288d5255e2bb12394
Poornima Rao <106062544+mangetsu711@users.noreply.github.com>
2026-07-06 17:32:26 +0530

    tag name changed to 0.0.0 for beta

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KhyaalCoreSDK",
-            url: "https://github.com/Khyaal-Inc/KhyaalCoreSDK/releases/download/1.0.1beta/KhyaalCoreSDK_iOS_v1.0.1.xcframework.zip",
+            url: "https://github.com/Khyaal-Inc/KhyaalCoreSDK/releases/download/0.0.0/KhyaalCoreSDK_iOS_v1.0.1.xcframework.zip",
             checksum: "a676f185a782c73b19410cfa1f6e55e8dff3898357339125c1680d6388ad299d"
         )
     ]
