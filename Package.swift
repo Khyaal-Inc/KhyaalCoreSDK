e3b8e2c53ea15d10e44b433a2a518b434d9d93dc
Poornima Rao <poornima@khyaal.com>
2026-07-07 01:00:11 +0530

    updated checksum and zip url

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KhyaalCoreSDK",
-            url: "https://github.com/Khyaal-Inc/KhyaalCoreSDK/releases/download/0.0.0/KhyaalCoreSDK_iOS_v1.0.1.xcframework.zip",
-            checksum: "a676f185a782c73b19410cfa1f6e55e8dff3898357339125c1680d6388ad299d"
+            url: "https://github.com/Khyaal-Inc/KhyaalCoreSDK/releases/download/0.0.0/KhyaalCoreSDK_v1.0.1.xcframework.zip",
+            checksum: "f754a1a18e69447e15e2d9cb065f595151981b89e124296efdea72c04dda8eb2"
         )
     ]
 )
