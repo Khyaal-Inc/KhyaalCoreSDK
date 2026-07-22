ab5d23d7f2d1c93758f58f899dce538b155920c7
Sanjeeb Samanta <63062092+Sanjeeb241@users.noreply.github.com>
2026-07-22 13:05:42 +0530

    Update KhyaalCoreSDK to version 1.0.6

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KhyaalCoreSDK",
-            url: "https://github.com/Khyaal-Inc/KhyaalCoreSDK/releases/download/0.0.0/KhyaalCoreSDK_v1.0.5.xcframework.zip",
-            checksum: "35f987ecfe264521314ca6eccef9e00cf64de1d28192a718a2cc4587d1fe59f2"
+            url: "https://github.com/Khyaal-Inc/KhyaalCoreSDK/releases/download/0.0.0/KhyaalCoreSDK_v1.0.6.xcframework.zip",
+            checksum: "8de5a8e81823a3d3c520e400c7780c00b9f886f3d616f27ba75e666d1bd607e"
         )
     ]
 )
