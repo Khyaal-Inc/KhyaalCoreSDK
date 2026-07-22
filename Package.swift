03aad6b14382a7f48faff1c7897679fc5d9247b5
Sanjeeb Samanta <63062092+Sanjeeb241@users.noreply.github.com>
2026-07-22 13:56:28 +0530

    Update checksum for KhyaalCoreSDK binary target

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .binaryTarget(
             name: "KhyaalCoreSDK",
             url: "https://github.com/Khyaal-Inc/KhyaalCoreSDK/releases/download/0.0.0/KhyaalCoreSDK_v1.0.6.xcframework.zip",
-            checksum: "8de5a8e81823a3d3c520e400c7780c00b9f886f3d616f27ba75e666d1bd607e"
+            checksum: "8de5a8e81823a3d3c520e400c7780c00b9f886f3d616f27ba75e666d1bd607e4"
         )
     ]
 )
